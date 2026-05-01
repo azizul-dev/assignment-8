88a06ee4be34c99b8d6fe0fab9be7a3a4d09586e
Md Azizul Islam <mdazizulislam@Mds-MacBook-Air.local>
2026-05-02 02:09:33 +0600

    add

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,8 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
+
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
-})
-export const { signIn, signUp, useSession } = createAuthClient()
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://assignment-8-eta-two.vercel.app"
+      : "http://localhost:3000",
+});
